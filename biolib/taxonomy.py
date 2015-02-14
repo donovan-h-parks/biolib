ca7bc92c64e1802df4a0be7d8d692488cdfc68df
Donovan Parks <donovan.parks@gmail.com>
2015-02-14 21:17:58 +1000

    Filled out functionality on Taxonomy() class.

--- biolib/taxonomy.py
+++ biolib/taxonomy.py
@@ -22,8 +22,14 @@ __license__ = 'GPL3'
 __maintainer__ = 'Donovan Parks'
 __email__ = 'donovan.parks@gmail.com'
 
+
+class InvalidTaxonomy(Exception):
+    """Exception thrown for invalid taxonomy string."""
+    pass
+    
+
 class Taxonomy(object):
-    """Manipulation of Greengenes-style taxonomy strings.
+    """Manipulation of Greengenes-style taxonomy files and strings.
 
     This class currently assumes a Greengenes-style taxonomy
     string with the following 7 taxonomic ranks:
@@ -38,32 +44,50 @@ class Taxonomy(object):
         self.rank_labels = ['domain', 'phylum', 'class', 'order', 'family', 'genus', 'species']
         self.rank_index = {'d__': 0, 'p__': 1, 'c__': 2, 'o__': 3, 'f__': 4, 'g__': 5, 's__': 6]}
         
-    def split(self, taxa_str):
-        """Split taxonomy string into ranks.
+    def taxa(self, tax_str):
+        """Taxa specified by taxonomy string.
         
         Parameters
         ----------
-        taxa_str : str
+        tax_str : str
             Greengenes-style taxonomy string.
         
         Returns
         -------
-        dict
+        list : [<domain>, <phylum>, ..., <species>]
+            Rank order list of taxa.
+        """
+        
+        taxa = [x.strip() for x in tax_str.split(';')]
+
+        for taxa
+        
+    def taxa_at_ranks(self, tax_str):
+        """Taxon at each taxonomic rank.
+        
+        Parameters
+        ----------
+        tax_str : str
+            Greengenes-style taxonomy string.
+        
+        Returns
+        -------
+        dict : d[rank_label] -> taxon
             Taxon at each taxonomic rank.
         """
         
-        taxa = [x.strip() for x in taxa_str.split(';')]
+        taxa = self.taxa(tax_str)
         
         d = {}
         for rank, taxon in enumerate(taxa):
             d[self.rank_labels[i]] = taxon
             
-    def check_full(self, taxa_str):
+    def check_full(self, tax_str):
         """Check if taxonomy string specifies all expected ranks.
         
         Parameters
         ----------
-        taxa_str : str
+        tax_str : str
             Greengenes-style taxonomy string.
         
         Returns
@@ -72,7 +96,7 @@ class Taxonomy(object):
             True if string contains all expected ranks, else False.
         """
         
-        taxa = [x.strip() for x in taxa_str.split(';')]
+        taxa = [x.strip() for x in tax_str.split(';')]
         if len(taxa) != len(self.rank_prefixes):
             return False
             
@@ -81,4 +105,81 @@ class Taxonomy(object):
                 return False
                 
         return True
+        
+    def fill_missing_ranks(self, tax_str):
+        """Fill in any missing ranks in a taxonomy string.
+        
+        This function assumes the taxonomy string lists
+        taxa in proper rank order, but that some ranks
+        may be missing.
+        
+        Parameters
+        ----------
+        tax_str : str
+            Greengenes-style taxonomy string.
+        
+        Returns
+        -------
+        str
+            Taxonomy string with prefixes for all ranks.
+        """
+        
+        taxa = [x.strip() for x in tax_str.split(';')]
+        
+        new_tax = []
+        cur_taxa_index = 0
+        for rank_prefix in self.rank_prefixes:
+            if taxa[cur_taxa_index][0:3] == rank_prefix:
+                cur_taxa_index.append(taxa[cur_taxa_index])
+                cur_taxa_index += 1
+            else:
+                new_tax.append(rank_prefix)
+                
+        return ';'.join(new_tax)
+        
+    def read(self, taxonomy_file, validate=False):
+        """Read Greengenes-style taxonomy file.
+        
+        Expected format is:
+            <id>\t<taxonomy string>
+        
+        where the taxonomy string has the formats:
+            d__; c__; o__; f__; g__; s__
+        
+        Parameters
+        ----------
+        taxonomy_file : str
+            Greengenes-style taxonomy file.
+        validate : boolean
+            Check if all taxonomy strings are valid.
+        
+        Returns
+        -------
+        dict : d[unique_id] -> taxonomy string
+            Taxonomy strings indexed by unique ids.
+            
+        Exceptions
+        ----------
+        InvalidTaxonomy 
+            Thrown if validate is True and an invalid 
+            taxonomy string is encountered.
+        """
+        
+        d = {}
+        for line in open(taxonomy_file):
+            line_split = line.split('\t')
+            unique_id = line_split[0]
+            
+            tax_str = line_split[1].rstrip()
+            if tax_str[-1] == ';':
+                # remove trailing semicolons which sometimes
+                # appear in Greengenes-style taxonomy files
+                tax_str = tax_str[0:-1]
+            
+            if validate and not self.check_full(tax_str):
+                raise InvalidTaxonomy("Invalid taxonomy string: %s" % tax_str)
+            
+            d[unique_id] = tax_str
+            
+        return d
         
