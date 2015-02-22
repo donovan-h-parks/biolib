58818ef2ab529095d2ce842942480b34bebb6d94
Donovan Parks <donovan.parks@gmail.com>
2015-02-22 20:39:04 +1000

    Update custom_help_formatter.py

--- biolib/misc/custom_help_formatter.py
+++ biolib/misc/custom_help_formatter.py
@@ -17,7 +17,7 @@
 
 __author__ = 'Donovan Parks'
 __copyright__ = 'Copyright 2014'
-__credits__ = ['Donovan Parks']
+__credits__ = ['Donovan Parks', 'Connor Skennerton']
 __license__ = 'GPL3'
 __maintainer__ = 'Donovan Parks'
 __email__ = 'donovan.parks@gmail.com'
