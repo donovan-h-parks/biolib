3490d632480e4e164095680a07881139feaa4856
dparks1134 <donovan.parks@gmail.com>
2021-03-03 01:00:16 +1000

    Version 0.1.8

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@ from setuptools import setup
 
 import os
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 
 def version():
     setupDir = os.path.dirname(os.path.realpath(__file__))
     versionFile = open(os.path.join(setupDir, 'biolib', 'VERSION'))
     return versionFile.readline().strip()
+
 
 setup(
     name='biolib',
@@ -18,5 +22,17 @@ setup(
     url='http://pypi.python.org/pypi/biolib/',
     license='GPL3',
     description='Package for common tasks in bioinformatic.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
     install_requires=[],
 )
